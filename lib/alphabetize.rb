194e25341b6dbaa33dba86199e81e9cdc058f03d
SOYEONG OH <axionoso@gmail.com>
2019-03-29 23:16:33 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,10 @@
 # ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word|
+    word.chars.map do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
