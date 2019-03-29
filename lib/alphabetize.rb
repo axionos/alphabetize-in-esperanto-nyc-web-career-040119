89f397e13d91e5273635504a0d8b5091c7909351
SOYEONG OH <axionoso@gmail.com>
2019-03-29 23:29:58 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,9 @@ def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
     word.chars.collect do |letter|
-      
+
     end
   end
 end
+
+"pacon", "mi amas vin", "bonan matenon", "ĉu vi parolas es"
